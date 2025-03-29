473cf547e6b46cbacb4a0f33d5c8af7db3240682
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-03-29 09:10:09 -0600

    crear modelo para paciente

---
+++ src/main/java/com/dentalcare/g5/main/model/entity/Paciente.java
@@ -0,0 +1,33 @@
+package com.dentalcare.g5.main.model.entity;
+
+import com.dentalcare.g5.main.model.entity.cita.Cita;
+import com.dentalcare.g5.main.model.entity.usuario.Usuario;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Paciente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private LocalDate fechaRegistro;
+
+    @OneToOne()
+    @JoinColumn(name = "usuario_id", referencedColumnName = "id")
+    private Usuario usuario;
+
+    @OneToMany(mappedBy = "paciente", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Cita> citas = new ArrayList<>();
+}
