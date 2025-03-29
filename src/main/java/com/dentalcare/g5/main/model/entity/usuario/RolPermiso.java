30c7e803e5fe8523cd135c4abf0f455ba77e13e5
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-03-29 09:10:02 -0600

    crear modelos relacionados a usuario

---
+++ src/main/java/com/dentalcare/g5/main/model/entity/usuario/RolPermiso.java
@@ -0,0 +1,26 @@
+package com.dentalcare.g5.main.model.entity.usuario;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class RolPermiso {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne()
+    @JoinColumn(name = "permiso_id", nullable = false)
+    private Permiso permiso;
+
+    @ManyToOne
+    @JoinColumn(name = "rol_id", nullable = false)
+    private Rol rol;
+}
---
+++ src/main/java/com/dentalcare/g5/main/model/entity/usuario/Usuario.java
@@ -0,0 +1,39 @@
+package com.dentalcare.g5.main.model.entity.usuario;
+
+import com.dentalcare.g5.main.model.entity.Paciente;
+import com.dentalcare.g5.main.model.entity.doctor.Doctor;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nombre;
+    private String apellido;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String telefono;
+
+    @OneToOne()
+    @JoinColumn(name = "rol_id", referencedColumnName = "id")
+    private Rol rol;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Paciente paciente;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Doctor doctor;
+}
