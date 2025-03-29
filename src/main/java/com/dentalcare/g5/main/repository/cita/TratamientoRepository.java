afec36bdc2e781b967bf2d35e3e1cbb2b5c293f3
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-03-29 09:10:22 -0600

    crear modelos relacionados a cita

---
+++ src/main/java/com/dentalcare/g5/main/repository/cita/TratamientoRepository.java
@@ -0,0 +1,7 @@
+package com.dentalcare.g5.main.repository.cita;
+
+import com.dentalcare.g5.main.model.entity.cita.Tratamiento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TratamientoRepository extends JpaRepository<Tratamiento, Integer> {
+}
