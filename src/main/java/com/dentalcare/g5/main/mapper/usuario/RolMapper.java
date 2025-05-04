ab63efe3cd8bf07a3c958f8a6f4093333c1c0d50
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-05-03 22:05:53 -0600

    añadir metodo para devolver un listado de dto

--- src/main/java/com/dentalcare/g5/main/mapper/usuario/RolMapper.java
+++ src/main/java/com/dentalcare/g5/main/mapper/usuario/RolMapper.java
@@ -4,8 +4,11 @@ import com.dentalcare.g5.main.model.dto.usuario.RolDto;
 import com.dentalcare.g5.main.model.entity.usuario.Rol;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface RolMapper {
     RolDto toDto(Rol rol);
     Rol toEntity(RolDto rolDto);
+    List<RolDto> toDtoList(List<Rol> roles);
 }
--- src/main/java/com/dentalcare/g5/main/mapper/usuario/UsuarioMapper.java
+++ src/main/java/com/dentalcare/g5/main/mapper/usuario/UsuarioMapper.java
@@ -4,8 +4,11 @@ import com.dentalcare.g5.main.model.dto.usuario.UsuarioDto;
 import com.dentalcare.g5.main.model.entity.usuario.Usuario;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface UsuarioMapper {
     UsuarioDto toDto(Usuario usuario);
     Usuario toEntity(UsuarioDto usuarioDto);
+    List<UsuarioDto> toDtoList(List<Usuario> usuarios);
 }
