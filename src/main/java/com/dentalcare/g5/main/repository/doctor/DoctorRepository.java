9bdfd153beaaf8ada2a7a28453cb6a9a176a74ab
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-05-03 22:07:34 -0600

    añadir metodo para buscar doctor por id

--- src/main/java/com/dentalcare/g5/main/repository/doctor/DoctorRepository.java
+++ src/main/java/com/dentalcare/g5/main/repository/doctor/DoctorRepository.java
@@ -4,6 +4,9 @@ import com.dentalcare.g5.main.model.entity.doctor.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface DoctorRepository extends JpaRepository<Doctor, Integer> {
+    Optional<Doctor> findByUsuarioId(Integer usuario_id);
 }
