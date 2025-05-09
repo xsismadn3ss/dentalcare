77a14a65bf5e969162aee5ff4c04e71c9cf218d8
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-05-08 20:13:54 -0600

    feat(mapper): add list mapping methods for Doctor and Especialidad
    This commit introduces new methods `toDtoList` in `DoctorMapper` and `toDto` in `EspecialidadMapper` to handle the conversion of lists of entities to lists of DTOs. This enhancement improves the functionality by allowing bulk mapping operations, which is useful for scenarios where multiple entities need to be processed at once.

--- src/main/java/com/dentalcare/g5/main/mapper/doctor/DoctorMapper.java
+++ src/main/java/com/dentalcare/g5/main/mapper/doctor/DoctorMapper.java
@@ -4,8 +4,11 @@ import com.dentalcare.g5.main.model.dto.doctor.DoctorDto;
 import com.dentalcare.g5.main.model.entity.doctor.Doctor;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface DoctorMapper {
     DoctorDto toDto(Doctor doctor);
+    List<DoctorDto> toDtoList(List<Doctor> doctors);
     Doctor toEntity(DoctorDto doctorDto);
 }
--- src/main/java/com/dentalcare/g5/main/mapper/doctor/EspecialidadMapper.java
+++ src/main/java/com/dentalcare/g5/main/mapper/doctor/EspecialidadMapper.java
@@ -4,8 +4,11 @@ import com.dentalcare.g5.main.model.dto.doctor.EspecialidadDto;
 import com.dentalcare.g5.main.model.entity.doctor.Especialidad;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface EspecialidadMapper {
     EspecialidadDto toDto(Especialidad especialidad);
+    List<EspecialidadDto> toDto(List<Especialidad> especialidades);
     Especialidad toEntity(EspecialidadDto especialidadDto);
 }
