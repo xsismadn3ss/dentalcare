3b0a941ae9f638419b8bb87489b2b563a7b88812
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-05-22 21:34:51 -0600

    refactor(auth): add Lombok annotations to LoginRequest class
    Simplify the LoginRequest class by adding Lombok annotations to reduce boilerplate code. This improves maintainability and readability by automatically generating getters, setters, and constructors.

--- src/main/java/com/dentalcare/g5/main/model/payload/auth/LoginRequest.java
+++ src/main/java/com/dentalcare/g5/main/model/payload/auth/LoginRequest.java
@@ -1,4 +1,13 @@
 package com.dentalcare.g5.main.model.payload.auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
+    private String username;
+    private String password;
 }
