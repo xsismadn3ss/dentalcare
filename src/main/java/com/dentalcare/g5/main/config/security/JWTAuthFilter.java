a004e6a35f23e4f7fe86e37901bec1732da3736d
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-05-22 22:48:04 -0600

    refactor(security): add @Component and @RequiredArgsConstructor to JWTAuthFilter
    This commit adds the @Component and @RequiredArgsConstructor annotations to the JWTAuthFilter class to improve dependency injection and reduce boilerplate code. The changes enhance maintainability and align with Spring best practices.

--- src/main/java/com/dentalcare/g5/main/config/security/JWTAuthFilter.java
+++ src/main/java/com/dentalcare/g5/main/config/security/JWTAuthFilter.java
@@ -11,17 +11,21 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.Collections;
 
+@Component
+@RequiredArgsConstructor
 public class JWTAuthFilter extends OncePerRequestFilter {
     @Autowired
     private JWTService jwtService;
