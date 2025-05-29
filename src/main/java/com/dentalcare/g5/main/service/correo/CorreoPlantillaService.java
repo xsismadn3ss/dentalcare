0212ad6254f82f45bb2394759a3cf833d818eff9
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-05-29 00:42:06 -0600

    agregar parametro para ubicacion del error

--- src/main/java/com/dentalcare/g5/main/service/correo/CorreoPlantillaService.java
+++ src/main/java/com/dentalcare/g5/main/service/correo/CorreoPlantillaService.java
@@ -3,5 +3,5 @@ package com.dentalcare.g5.main.service.correo;
 import java.util.Optional;
 
 public interface CorreoPlantillaService {
-    String crearPlantillaError(String error_name, String details, String url);
+    String crearPlantillaError(String error_name, String details, String ubicacion, String url);
 }
