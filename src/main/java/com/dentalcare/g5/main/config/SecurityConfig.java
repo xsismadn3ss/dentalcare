d008281d6aeaf75296ef2ea02e4ec13c39956cf2
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-05-29 02:11:03 -0600

    crear configuracion para añadir filters

--- src/main/java/com/dentalcare/g5/main/config/SecurityConfig.java
+++ src/main/java/com/dentalcare/g5/main/config/SecurityConfig.java
@@ -1,4 +1,41 @@
 package com.dentalcare.g5.main.config;
 
+import com.dentalcare.g5.main.security.JWTAuthenticationFilter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Slf4j
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    private JWTAuthenticationFilter jwtAuthenticationFilter;
+
+    @Value("/${server.base}/${server.version}")
+    private String path_prefix;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        log.info("{}/auth/login", path_prefix);
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                path_prefix + "/auth/login"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
+    }
 }
